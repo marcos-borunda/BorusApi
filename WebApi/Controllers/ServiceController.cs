a9c4b0f46e532df91523e4f38b6c0b1b4e21d33c
Marcos Borunda <marcos.borunda@gmail.com>
2020-05-26 11:54:45 -0600

    Fixing indentation

--- WebApi/Controllers/ServiceController.cs
+++ WebApi/Controllers/ServiceController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -8,40 +7,38 @@ using WebApi.BusinessLogic;
 
 namespace WebApi.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  public class ServiceController : ControllerBase
-  {
-    private readonly ICommandTranslator commandTranslator;
-    private readonly ILogger<ServiceController> logger;
-
-    public ServiceController(ICommandTranslator commandTranslator, ILogger<ServiceController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceController : ControllerBase
     {
-      this.commandTranslator = commandTranslator ?? throw new ArgumentNullException(nameof(commandTranslator));
-      this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        private readonly ICommandTranslator commandTranslator;
+        private readonly ILogger<ServiceController> logger;
+
+        public ServiceController(ICommandTranslator commandTranslator, ILogger<ServiceController> logger)
+        {
+            this.commandTranslator = commandTranslator ?? throw new ArgumentNullException(nameof(commandTranslator));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IActionResult Get(string statement)
+        {
+            var commandWords = statement.Split(' ').ToList();
+
+            if (commandWords.Count < 2)
+                return BadRequest($"{nameof(statement)}: '{statement}' is not valid. It should have at least 2 words (service and command).");
+
+            var command = this.commandTranslator.Translate(commandWords);
+
+            if (command is null)
+                return NotFound($"{nameof(statement)}: '{statement}' is not valid. Service was not found.");
+
+            var response = new Invoker(command).Invoke();
+
+            if (response.Status == StatusResponse.Error)
+                return StatusCode(StatusCodes.Status500InternalServerError, $"{nameof(statement)}: '{statement}'. Error while executing.");
+
+            return Ok(response.Message);
+        }
     }
-
-    [HttpGet]
-    public IActionResult Get(string statement)
-    {
-        var commandWords = statement.Split(' ').ToList();
-
-        if (commandWords.Count < 2)
-            return BadRequest($"{nameof(statement)}: '{statement}' is not valid. It should have at least 2 words (service and command).");
-
-        var command = this.commandTranslator.Translate(commandWords);
-
-        if (command is null)
-            return NotFound($"{nameof(statement)}: '{statement}' is not valid. Service was not found.");
-        
-        var invoker = new Invoker(command);
-
-        var response = invoker.Invoke();
-
-        if (response.Status == StatusResponse.Error)
-            return StatusCode(StatusCodes.Status500InternalServerError, $"Error while executing {nameof(statement)} '{statement}'.");
-
-        return Ok(response.Message);
-    }
-  }
 }
